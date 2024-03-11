f2761efe383194c60a32806e897855f62ae9ee20
Mustapha Ojo <ojomustapha@gmail.com>
2024-03-11 11:49:05 +0100

    Revert "Add svelte adapter auto"
    This reverts commit a7748161c29bf48a9ad881f5ff3302bc11e89e6b.

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/entries/pages/check_email/_page.svelte.js
+++
@@ -1,7 +0,0 @@
-import { c as create_ssr_component } from "../../../chunks/ssr.js";
-const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
-  return `<p data-svelte-h="svelte-5i1gfi">Check your email to confirm.</p>`;
-});
-export {
-  Page as default
-};
--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/2.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 2;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_page.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/2.xIU87iF4.js","_app/immutable/chunks/scheduler.HbwKJWOp.js","_app/immutable/chunks/index.8g-ltFm-.js","_app/immutable/chunks/Frame.fKDvhI-6.js","_app/immutable/chunks/index.HtXAd2fM.js","_app/immutable/chunks/CloseButton.6k_5TkK5.js"];
-export const stylesheets = [];
-export const fonts = [];
